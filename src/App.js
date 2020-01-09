69347da6d84e2ba6f8010ae222499952cf0d5713
Admin <raju.chapagain@outlook.com>
2020-01-09 20:33:32 +1100

    update menu ite to navigate with router HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React from "react";
+import { Route, Link, Switch } from "react-router-dom";
+
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 
+const HatsPage = () => (
+  <div>
+    <h1>Hats page</h1>
+    <Link to="/">Home</Link>
+  </div>
+);
+const HatsPageWithId = props => (
+  <div>
+    <button onClick={() => props.history.push("/")} className="Button">
+      Home
+    </button>
+    <h1>Hats pageid:{props.match.params.id}</h1>
+    <Link to="/">Home</Link>
+  </div>
+);
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HatsPage} />
+        <Route exact path="/hats/:id" component={HatsPageWithId} />
+      </Switch>
     </div>
   );
 }
