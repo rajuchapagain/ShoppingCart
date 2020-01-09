bcc651219e78054fa5422a956ddee85416eb1b6a
Admin <raju.chapagain@outlook.com>
2020-01-10 00:20:12 +1100

    collection item components

--- src/App.js
+++ src/App.js
@@ -4,29 +4,14 @@ import { Route, Link, Switch } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
-const HatsPage = () => (
-  <div>
-    <h1>Hats page</h1>
-    <Link to="/">Home</Link>
-  </div>
-);
-const HatsPageWithId = props => (
-  <div>
-    <button onClick={() => props.history.push("/")} className="Button">
-      Home
-    </button>
-    <h1>Hats pageid:{props.match.params.id}</h1>
-    <Link to="/">Home</Link>
-  </div>
-);
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/hats" component={HatsPage} />
-        <Route exact path="/hats/:id" component={HatsPageWithId} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
